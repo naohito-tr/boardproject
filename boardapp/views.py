1b881fe3f2f9d8492ece48c6a32be30f4c4c449c
naohito-tr <trueNtears@gmail.com>
2020-12-01 21:42:41 +0900

    boradproject

---
+++ boardapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from .models import BoardModel
+
+# Create your views here.
+def signupfunc(request):
+    user2 = User.objects.get(username='naohito')
+    print(user2.email)
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html', {'some': 100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list':object_list})
